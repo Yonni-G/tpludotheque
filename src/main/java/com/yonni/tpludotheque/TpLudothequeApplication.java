b1064350d3fc4555ec041eb4249b87a9b53dc28e
Yonni GUERRAULT <yonni4@gmail.com>
2024-12-23 11:29:33 +0100

    init

---
+++ src/main/java/com/yonni/tpludotheque/TpLudothequeApplication.java
@@ -0,0 +1,13 @@
+package com.yonni.tpludotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpLudothequeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpLudothequeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yonni/tpludotheque/TpLudothequeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yonni.tpludotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpLudothequeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
